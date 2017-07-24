de2fdc7e9e46d2de99244c9d156f6a8142133516
Vitaly _Vi Shukela <vi0oss@gmail.com>
2017-07-24 03:12:15 +0300

    Refactor&fix

--- udpnat.c
+++ udpnat.c
@@ -81,6 +81,108 @@ static int expire_connections() {
     return -1;
 }
 
+
+static void serve_tundev(int tundev) {
+    int ret;
+    for(;;) {
+        struct sockaddr_in src;
+        struct sockaddr_in dst;
+        
+        uint8_t *data;
+        
+        ret = receive_udp_packet_from_tun(
+                tundev, 
+                (char*)buf, sizeof buf, 
+                &src, &dst, 
+                (char**)&data);
+        
+        if (-1 == ret) {
+            if (errno == EAGAIN) return;
+            if (errno == EINTR || 
+                errno == EPROTONOSUPPORT ||
+                errno == ESOCKTNOSUPPORT || 
+                errno == EOPNOTSUPP) continue;
+            return;
+        }
+        
+        if (src.sin_addr.s_addr == 0) continue;
+        
+        int s = find_connection(src);
+        if (s == -1) {
+            fprintf(stderr, "New connection: %s:%d -> ",inet_ntoa(src.sin_addr), ntohs(src.sin_port));
+            fprintf(stderr, "%s:%d. Created socket ", inet_ntoa(dst.sin_addr), ntohs(dst.sin_port));
+            s = socket(AF_INET, SOCK_DGRAM, 0);
+            
+            if (s == -1) continue; // FIXME: send ICMP dest unreach
+            // Optional bind to port
+            struct sockaddr_in sbind;
+            memset(&sbind, 0, sizeof(sbind));
+            sbind.sin_family = AF_INET;
+            sbind.sin_addr.s_addr = 0;
+            sbind.sin_port = src.sin_port;
+            bind(s, (struct sockaddr*)&sbind, sizeof(sbind));
+            
+            fprintf(stderr, "%d\n", s);
+            if (s >= MAXCONNS) {
+                close(s); // FIXME: send ICMP dest unreach
+            }
+            connections[s].src_ip = src.sin_addr.s_addr;
+            connections[s].src_port = src.sin_port;
+            connections[s].ttl = TTL;
+            fcntl(s, F_SETSIG, SIGRTMIN);
+            fcntl(s, F_SETOWN, getpid());
+            fcntl(s, F_SETFL, O_ASYNC|O_RDWR|O_NONBLOCK);
+            
+            uint8_t psmi = (src.sin_port >> 8) ^ (src.sin_port & 0xFF);
+            pseudomap[psmi] = s;
+        } else {
+            //printf(stderr, "Old connection: %s:%d -> ",inet_ntoa(src.sin_addr), ntohs(src.sin_port));
+            //fprintf(stderr, "%s:%d. Associated socket", inet_ntoa(dst.sin_addr), ntohs(dst.sin_port));
+            //fprintf(stderr, " is %d\n", s);
+            connections[s].ttl = TTL;
+        }
+        
+        sendto(s, data, ret, 0, (struct sockaddr*)&dst, sizeof(dst));
+    }
+}
+
+static void serve_sock(int fd, int tundev) {
+    int ret;
+    if (fd >= MAXCONNS || fd < 0) {
+        fprintf(stderr, "Invalid FD from signal: %d\n", fd);
+        return;
+    }
+    
+    struct connection* c = connections+fd;
+    //fprintf(stderr, "Incoming data on fd %d\n", fd);
+    
+    struct sockaddr_in dst;
+    memset(&dst, 0, sizeof(dst));
+    dst.sin_family = AF_INET;
+    dst.sin_addr.s_addr = c->src_ip;
+    dst.sin_port = c->src_port;
+    
+    for(;;) {
+        struct sockaddr_in src;
+        socklen_t src_len = sizeof(src);
+        
+        ret = recvfrom(fd, buf, sizeof buf, 0, (struct sockaddr*)&src, &src_len);
+        
+        if (ret == -1) {
+            if (errno == EAGAIN) return;
+            // else just wait for socket to be closed
+            return;
+        }
+        
+        // Reply to tun
+        send_udp_packet_to_tun(
+                tundev, (char*)buf_reply, sizeof(buf_reply),
+                &src, &dst,
+                (const char*)buf, ret,
+                &ip_id);
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc!=4 || !strcmp(argv[1], "--help") || !strcmp(argv[1], "-?")) {
         printf("Usage: udpnat /dev/net/tun ifname expire_seconds\n");
@@ -100,6 +202,7 @@ int main(int argc, char* argv[]) {
     sigemptyset(&sigs);
     sigaddset(&sigs, SIGRTMIN);
     sigaddset(&sigs, SIGALRM);
+    sigaddset(&sigs, SIGIO);
     sigprocmask(SIG_BLOCK, &sigs, NULL);
     
     fcntl(tundev, F_SETOWN, getpid());
@@ -112,7 +215,6 @@ int main(int argc, char* argv[]) {
     
     int ret;
     
-    continue_to_next_signal:
     for(;;) {
         siginfo_t si;
         ret = sigwaitinfo(&sigs, &si);
@@ -122,103 +224,24 @@ int main(int argc, char* argv[]) {
             alarm(SCAN_INTERVAL);
         }
         
+        if (ret == SIGIO) {
+            fprintf(stderr, "Lost some signals?\n");
+            serve_tundev(tundev);
+            int i;
+            for(i=0; i<MAXCONNS; ++i) {
+                if (connections[i].src_ip != 0) {
+                    serve_sock(i, tundev);
+                }
+            }
+        }
+        
         if (ret != SIGRTMIN) continue;
         
+        
         if (si.si_fd == tundev) {
-            for(;;) {
-                struct sockaddr_in src;
-                struct sockaddr_in dst;
-                
-                uint8_t *data;
-                
-                ret = receive_udp_packet_from_tun(
-                        tundev, 
-                        (char*)buf, sizeof buf, 
-                        &src, &dst, 
-                        (char**)&data);
-                
-                if (-1 == ret) {
-                    if (errno == EAGAIN) goto continue_to_next_signal;
-                    if (errno == EINTR || 
-                        errno == EPROTONOSUPPORT ||
-                        errno == ESOCKTNOSUPPORT || 
-                        errno == EOPNOTSUPP) continue;
-                    return 3;
-                }
-                
-                if (src.sin_addr.s_addr == 0) continue;
-                
-                int s = find_connection(src);
-                if (s == -1) {
-                    fprintf(stderr, "New connection: %s:%d -> ",inet_ntoa(src.sin_addr), ntohs(src.sin_port));
-                    fprintf(stderr, "%s:%d. Created socket ", inet_ntoa(dst.sin_addr), ntohs(dst.sin_port));
-                    s = socket(AF_INET, SOCK_DGRAM, 0);
-                    
-                    if (s == -1) continue; // FIXME: send ICMP dest unreach
-                    // Optional bind to port
-                    struct sockaddr_in sbind;
-                    memset(&sbind, 0, sizeof(sbind));
-                    sbind.sin_family = AF_INET;
-                    sbind.sin_addr.s_addr = 0;
-                    sbind.sin_port = src.sin_port;
-                    bind(s, (struct sockaddr*)&sbind, sizeof(sbind));
-                    
-                    fprintf(stderr, "%d\n", s);
-                    if (s >= MAXCONNS) {
-                        close(s); // FIXME: send ICMP dest unreach
-                    }
-                    connections[s].src_ip = src.sin_addr.s_addr;
-                    connections[s].src_port = src.sin_port;
-                    connections[s].ttl = TTL;
-                    fcntl(s, F_SETSIG, SIGRTMIN);
-                    fcntl(s, F_SETOWN, getpid());
-                    fcntl(s, F_SETFL, O_ASYNC|O_RDWR|O_NONBLOCK);
-                    
-                    uint8_t psmi = (src.sin_port >> 8) ^ (src.sin_port & 0xFF);
-                    pseudomap[psmi] = s;
-                } else {
-                    //printf(stderr, "Old connection: %s:%d -> ",inet_ntoa(src.sin_addr), ntohs(src.sin_port));
-                    //fprintf(stderr, "%s:%d. Associated socket", inet_ntoa(dst.sin_addr), ntohs(dst.sin_port));
-                    //fprintf(stderr, " is %d\n", s);
-                    connections[s].ttl = TTL;
-                }
-                
-                sendto(s, data, ret, 0, (struct sockaddr*)&dst, sizeof(dst));
-            }
+            serve_tundev(tundev);
         } else {
-            if (si.si_fd >= MAXCONNS || si.si_fd < 0) {
-                fprintf(stderr, "Invalid FD from signal: %d\n", si.si_fd);
-                continue;
-            }
-            
-            struct connection* c = connections+si.si_fd;
-            //fprintf(stderr, "Incoming data on fd %d\n", si.si_fd);
-            
-            struct sockaddr_in dst;
-            memset(&dst, 0, sizeof(dst));
-            dst.sin_family = AF_INET;
-            dst.sin_addr.s_addr = c->src_ip;
-            dst.sin_port = c->src_port;
-            
-            for(;;) {
-                struct sockaddr_in src;
-                socklen_t src_len = sizeof(src);
-                
-                ret = recvfrom(si.si_fd, buf, sizeof buf, 0, (struct sockaddr*)&src, &src_len);
-                
-                if (ret == -1) {
-                    if (errno == EAGAIN) goto continue_to_next_signal;
-                    // else just wait for socket to be closed
-                    goto continue_to_next_signal;
-                }
-                
-                // Reply to tun
-                send_udp_packet_to_tun(
-                        tundev, (char*)buf_reply, sizeof(buf_reply),
-                        &src, &dst,
-                        (const char*)buf, ret,
-                        &ip_id);
-            }
+            serve_sock(si.si_fd, tundev);
         }
     }
    
